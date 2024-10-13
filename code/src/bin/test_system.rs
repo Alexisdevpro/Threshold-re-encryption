c17c3dc1c4cf7cf95f0bb1b2dcfeb1a282f6c3f0
Alexis <43484468+Alexisdevpro@users.noreply.github.com>
2024-10-13 21:41:15 +0200

    add test

--- code/src/bin/test_system.rs
+++ code/src/bin/test_system.rs
@@ -3,7 +3,7 @@ use std::fs::{self, create_dir_all, File, remove_dir_all, remove_file};
 use std::io::{BufWriter, Read, Write};
 use std::path::Path;
 use tfhe::core_crypto::prelude::*;
-use crate::lwe_functions::{generate_alea_encrypt, keygen, PublicKey};
+use crate::lwe_functions::{ecrypt_text, generate_alea_encrypt, keygen, PublicKey};
 use crate::secret_sharing::shared_vector_for_two_machines;
 use crate::from_file::{append_vectors_to_files, keys_from_file};
 
@@ -56,12 +56,18 @@ fn encrypt_plaintext_from_file() {
     let (public_key, _) = keys_from_file("test_key/key_sys.txt").unwrap();
     let mut plaintext_file = File::open("plaintext/plaintext.txt").unwrap();
     let mut plaintext_content = String::new();
-    plaintext_file.read_to_string(&mut plaintext_content).unwrap();
-    let text = plaintext_content.trim().parse::<i64>().unwrap();
+    plaintext_file.read_to_string(&mut plaintext_content);
 
-    let ciphertext = lwe_functions::encrypt(&public_key, text);
-    let mut ciphertext_file = File::create("ciphertexts/ciphertexts.txt").unwrap();
-    ciphertext_file.write_all(&format!("{:?}", ciphertext).into_bytes()).unwrap();
+    if let Ok(number) = plaintext_content.trim().parse::<i64>() {
+        let ciphertext = lwe_functions::encrypt(&public_key, number);
+        let mut ciphertext_file = File::create("ciphertexts/ciphertexts.txt").unwrap();
+        ciphertext_file.write_all(&format!("{:?}", ciphertext).into_bytes()).unwrap();
+    } else {
+        let text = plaintext_content.trim();
+        let ciphertext = ecrypt_text(&public_key, text);
+        let mut ciphertext_file = File::create("ciphertexts/ciphertexts.txt").unwrap();
+        ciphertext_file.write_all(&format!("{:?}", ciphertext).into_bytes()).unwrap();
+    }
 }
 
 
