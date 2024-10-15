06b960d8ac4958b2c1ce4eea57a3fb4e76e232b5
Alexis <43484468+Alexisdevpro@users.noreply.github.com>
2024-10-15 18:43:06 +0200

    add comments

--- code/src/bin/test_system.rs
+++ code/src/bin/test_system.rs
@@ -16,21 +16,25 @@ const ALICEKEY: &str = "keys/cleAlice.txt";
 const BOBKEY: &str = "keys/cleBob.txt";
 
 
+//test1
 fn generate_system_keys_and_shares() {
     let path = Path::new("test_key");
     if path.exists() { remove_dir_all(path).expect("pb suppression"); }
     create_dir_all(path).expect("pb creation");
 
-
+    // Key generation and encryption randomness generation
     let (pk, sk) = keygen();
     let (r, _, _) = generate_alea_encrypt();
 
+    // Convert the secret key and random values to vectors of 64-bit integers.
     let secret_vector: Vec<i64> = sk.s.iter().map(|&x| x as i64).collect();
     let r_vector: Vec<i64> = r.iter().map(|&x| x as i64).collect();
 
+    // Split the secret vector and random vector into shares for two machines. (secret_sharing.rs)
     let (shares_machine_1, shares_machine_2) = shared_vector_for_two_machines(secret_vector, 2, Q as i64, 2);
     let (r_for_machine_1, r_for_machine_2) = shared_vector_for_two_machines(r_vector, 2, Q as i64, 10);
 
+    // Save the generated keys and shares into respective files.
     append_vectors_to_files(
         vec![
             ("test_key/key_sys.txt", vec![&pk.a.data.as_vec(), &pk.b.data.as_vec(), &sk.s.data.as_vec()]),
@@ -38,13 +42,18 @@ fn generate_system_keys_and_shares() {
             ("test_key/machine2.txt", vec![&shares_machine_2.data.as_vec(), &r_for_machine_2.data.as_vec()]),
         ]
     );
+
 }
 
+//test2
+// Generates a new key pair for Bob and saves it to a file.
+// If a previous key file exists, it removes it before generating a new key.
 pub fn generate_bob_key() {
     let (pk, sk) = keygen();
     let path = Path::new("keys");
     if path.exists() { remove_file(BOBKEY).expect("pb suppression"); }
 
+    // Save the generated public and secret keys to a file.
     append_vectors_to_files(
         vec![
             ("test_key/keypair_bob.txt", vec![&pk.a.data.as_vec(), &pk.b.data.as_vec(), &sk.s.data.as_vec()]),
@@ -52,6 +61,9 @@ pub fn generate_bob_key() {
     );
 }
 
+//test3
+// Encrypts plaintext data from a file using the public key system.
+// The plaintext can be either a numeric value or a text string.
 fn encrypt_plaintext_from_file() {
     let (public_key, _) = keys_from_file("test_key/key_sys.txt").unwrap();
     let mut plaintext_file = File::open("plaintext/plaintext.txt").unwrap();
