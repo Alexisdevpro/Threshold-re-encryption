c08ce05a56451be6758c77ab16665846e179bf4f
Alexis <43484468+Alexisdevpro@users.noreply.github.com>
2024-10-16 21:09:59 +0200

    rename files

--- code/src/bin/test_system.rs
+++ code/src/bin/test_system.rs
@@ -1,4 +1,3 @@
-
 use std::fs::{self, create_dir_all, File, remove_dir_all, remove_file};
 use std::io::{BufWriter, Read, Write};
 use std::path::Path;
@@ -7,53 +6,52 @@ use crate::lwe_functions::{ecrypt_text, generate_alea_encrypt, keygen, PublicKey
 use crate::secret_sharing::shared_vector_for_two_machines;
 use crate::from_file::{append_vectors_to_files, keys_from_file};
 
-
 mod secret_sharing;
 mod lwe_functions;
 mod from_file;
+
 const Q: f64 = (2 << 15) as f64;
 const ALICEKEY: &str = "keys/cleAlice.txt";
 const BOBKEY: &str = "keys/cleBob.txt";
 
-
-//test1
 fn generate_system_keys_and_shares() {
     let path = Path::new("test_key");
     if path.exists() { remove_dir_all(path).expect("pb suppression"); }
     create_dir_all(path).expect("pb creation");
 
-    // Key generation and encryption randomness generation
     let (pk, sk) = keygen();
     let (r, _, _) = generate_alea_encrypt();
 
-    // Convert the secret key and random values to vectors of 64-bit integers.
     let secret_vector: Vec<i64> = sk.s.iter().map(|&x| x as i64).collect();
     let r_vector: Vec<i64> = r.iter().map(|&x| x as i64).collect();
 
-    // Split the secret vector and random vector into shares for two machines. (secret_sharing.rs)
     let (shares_machine_1, shares_machine_2) = shared_vector_for_two_machines(secret_vector, 2, Q as i64, 2);
     let (r_for_machine_1, r_for_machine_2) = shared_vector_for_two_machines(r_vector, 2, Q as i64, 10);
 
-    // Save the generated keys and shares into respective files.
+    append_vectors_to_files(
+        vec![
+            ("test_key/share_machine1.txt", vec![&shares_machine_1.data.as_vec()]),
+            ("test_key/r_share_machine1.txt", vec![&r_for_machine_1.data.as_vec()]),
+        ]
+    );
+    append_vectors_to_files(
+        vec![
+            ("test_key/share_machine2.txt", vec![&shares_machine_2.data.as_vec()]),
+            ("test_key/r_share_machine2.txt", vec![&r_for_machine_2.data.as_vec()]),
+        ]
+    );
     append_vectors_to_files(
         vec![
             ("test_key/key_sys.txt", vec![&pk.a.data.as_vec(), &pk.b.data.as_vec(), &sk.s.data.as_vec()]),
-            ("test_key/machine1.txt", vec![&shares_machine_1.data.as_vec(), &r_for_machine_1.data.as_vec()]),
-            ("test_key/machine2.txt", vec![&shares_machine_2.data.as_vec(), &r_for_machine_2.data.as_vec()]),
         ]
     );
-
 }
 
-//test2
-// Generates a new key pair for Bob and saves it to a file.
-// If a previous key file exists, it removes it before generating a new key.
 pub fn generate_bob_key() {
     let (pk, sk) = keygen();
     let path = Path::new("keys");
     if path.exists() { remove_file(BOBKEY).expect("pb suppression"); }
 
-    // Save the generated public and secret keys to a file.
     append_vectors_to_files(
         vec![
             ("test_key/keypair_bob.txt", vec![&pk.a.data.as_vec(), &pk.b.data.as_vec(), &sk.s.data.as_vec()]),
@@ -61,14 +59,11 @@ pub fn generate_bob_key() {
     );
 }
 
-//test3
-// Encrypts plaintext data from a file using the public key system.
-// The plaintext can be either a numeric value or a text string.
 fn encrypt_plaintext_from_file() {
     let (public_key, _) = keys_from_file("test_key/key_sys.txt").unwrap();
     let mut plaintext_file = File::open("plaintext/plaintext.txt").unwrap();
     let mut plaintext_content = String::new();
-    plaintext_file.read_to_string(&mut plaintext_content);
+    plaintext_file.read_to_string(&mut plaintext_content).unwrap();
 
     if let Ok(number) = plaintext_content.trim().parse::<i64>() {
         let ciphertext = lwe_functions::encrypt(&public_key, number);
@@ -82,12 +77,9 @@ fn encrypt_plaintext_from_file() {
     }
 }
 
-
-
 fn main() {
     generate_system_keys_and_shares();
     generate_bob_key();
-    encrypt_plaintext_from_file()
+    encrypt_plaintext_from_file();
 }
-
 
